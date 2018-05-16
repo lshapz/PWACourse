2b4cdb9c7242d94844a96c4bf439ee58c23b10cb
Laura Shapiro <laurashapiro01@gmail.com>
2018-05-16 12:21:27 -0400

    finished section 2

--- course-project-starting-setup/public/src/icon_names.js
+++ course-project-starting-setup/public/src/icon_names.js
@@ -1,16 +1,40 @@
 const folder = './images/icons';
 const fs = require('fs');
 
+var appleArray = []
+
 var nameArray = fs.readdirSync(folder).map(item=>{
 	var iconObj = {}
+	var apple = item.split('-')[0] === 'apple'
 	var [size, type] = item.split('-icon-')[1].split('.')
+	var path = "/src/images/icons/" + item
+	if (!apple) {	
 
-	iconObj.src = "/src/images/icons/" + item
-	iconObj.type = "image/" + type
-	iconObj.sizes = size
+		iconObj["src"] = path
+		iconObj["type"] = "image/" + type
+		iconObj["sizes"] = size
+		return iconObj
 
-	return iconObj
+	} else {
+		appleArray.push(`<link rel="apple-touch-icon" href="/src/images/icons/${item}" sizes="${size}">`)
+	}
+	
+}).filter(item=>item)
 
-})
+fs.writeFile("./object.json", JSON.stringify(nameArray, null, 4), (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    };
+    console.log("File 1 has been created");
+});
 
-console.log(nameArray)
+
+var appleString = appleArray.map(item=>{return item + "\n"}).join('')
+var appleLinks = fs.createWriteStream('appleLinks.txt');
+appleLinks.on('error', function(err) { console.error(err); });
+
+appleLinks.write(appleString)
+console.log("File 2 has been created");
+appleLinks.end();
+
